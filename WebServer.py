831ddfa235fa281af6c7e89be3e63cd62d604e1d
olafkotur <olafkotur97@gmail.com>
2019-02-20 21:23:31 +0000

    Server responds with a file

--- WebServer.py
+++ WebServer.py
@@ -11,71 +11,9 @@ MAX_CONNECTIONS = 4
 FIXED_PORT = True
 
 
-def handleRequest(tcpSocket):
-	# Receive request message from the client on connection socket
-	data = tcpSocket.recv(1024).decode()
-	
-	# Extract useful infromation for the HTTP header
-	data = data.split(' ')
-	request = data[1]
-	print 'Client requested file ' + str(request) + ' from disk'
-
-	# TESTING ONLY - shutdown server if request is close
-	status = 200
-	if (request == '/close'): status = 9999
-
-	# 3. Read the corresponding file from disk
-	# 4. Store in temporary buffer
-	# 5. Send the correct HTTP response error
-	# 6. Send the content of the file to the socket
-	# 7. Close the connection socket 
-	return status
-
-def startServer(serverAddress, serverPort):
-	# Create server socket
-	tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	# Bind the server socket to server address and server port, increment until successful
-	portBound = False
-	while not portBound:
-		try: 
-			tcpSocket.bind((serverAddress, serverPort))
-			portBound = True
-		except:
-			print 'PORT ' + str(serverPort - 1) + ' is already in use\nAttempting PORT ' + str(serverPort)
-			serverPort += 1
-
-
-	# Listen for connections to server socket
-	tcpSocket.listen(MAX_CONNECTIONS)
-	print 'Listening on ' + str(serverAddress) + ' | PORT: ' + str(serverPort) + '\n'
-	startedTime = time.time()
-
-	# Keep server running
-	serverActive = True
-	while serverActive:
-		# Accept new connections
-		connectionSocket, address = tcpSocket.accept()
-
-		# Handle request if connection is accepted
-		if (connectionSocket):
-			print 'Received a connection at: ' + str(address[0]) + ' | ' + str(address[1]) + '\n'
-			
-			# Return status code from request
-			status = handleRequest(connectionSocket)
-			
-			# Close server if request is satisfied
-			if (status == 200):
-				print 'Request Satisfied\n'
-			elif (status == 404):
-				print 'This is not the file you are looking for'
-			elif (status == 9999): serverActive = False
-	
-	# Useful info
-	tcpSocket.close()
-	ranFor = time.time() - startedTime
-	print '\nServer ran for: ' + '%.3f' % ranFor + ' seconds.'
-
+def main():
+	port = userInput()
+	startServer(SERVER_ADDRESS, port)
 
 # Takes user input, if user skips then port set to random
 def userInput():
@@ -94,14 +32,98 @@ def userInput():
 			print 'Port number must be higher than 1024'
 			quit()
 	# Set port value if skipped
-	else: print '! SKIPPING: Setting PORT to ' + str(port)
+	else: print '! SKIPPING: Setting PORT to ' + str(port) + '\n'
 
 	return int(port)
 
-def main():
-	port = userInput()
-	startServer(SERVER_ADDRESS, port)
+
+def startServer(serverAddress, serverPort):
+	# Create server socket
+	tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Bind the server socket to server address and server port, increment port until successful
+	portBound = False
+	while not portBound:
+		try: 
+			tcpSocket.bind((serverAddress, serverPort))
+			portBound = True
+		except:
+			serverPort += 1
+			print 'PORT ' + str(serverPort - 1) + ' is already in use, attempting to connect using PORT ' + str(serverPort)
+
+	# Listen for connections to server socket
+	tcpSocket.listen(MAX_CONNECTIONS)
+	print 'Listening on ' + str(serverAddress) + ' | PORT: ' + str(serverPort) + '\n'
+	startedTime = time.time()
+
+	# Accept new connections
+	connectionSocket, address = tcpSocket.accept()
+
+	# Handle request if connection is accepted
+	if (connectionSocket):
+		print 'Received a connection at: ' + str(address[0]) + ' | ' + str(address[1])
+		
+		# Handle status code from client
+		status = handleRequest(connectionSocket)
+		if (status == 200): print '200 OK'
+		elif (status == 404): print '404 Not Found\n'
+	
+	# Useful info
+	tcpSocket.close()
+	ranFor = time.time() - startedTime
+	print '\nServer ran for: ' + '%.3f' % ranFor + ' seconds.'
+	sys.exit()
+
+def handleRequest(tcpSocket):
+	# Receive request message from the client on connection socket
+	data = tcpSocket.recv(1024).decode()
+	
+	# Extract useful infromation for the HTTP header
+	print data
+	data = data.split(' ')
+	method, request = data[0], data[1]
+
+	if (method == 'GET'):	
+		# Set default file to index.html
+		if (request == '/'): request = '/index.html'
+		print 'Client requested file ' + str(request) + ' from disk\n'
+		
+		# Read the corresponding file from disk
+		try: 
+			# Grab the file
+			file = open(request[1:], 'r')
+			requestedData = file.read()
+			file.close()
+			
+			# Pack the file into the header
+			header = createHeader(' ' + str(200) + ' OK')
+			requestedData = header + requestedData.encode()
+			tcpSocket.send(requestedData)
+			status = 200
+		except:
+			status = 404
 
 
+	# 6. Send the content of the file to the sock8et
+	# 7. Close the connection socket
+
+	# TESTING ONLY - shutdown server if request is close
+	if (request == '/close'): status = 9999
+	
+	return status
+
+
+# Generates header according to the provided code
+def createHeader(code):
+	formattedTime = time.strftime('%a, %d %b %Y %H:%M:%S', time.localtime())
+	header = 'HTTP/1.1 ' + code + '\n'
+	header += 'Date: ' + formattedTime + '\n'
+	header += 'Server: WebServer Example\n'
+	print header
+	print header.encode()
+	return header.encode()
+
+
+# Execute main
 if __name__ == "__main__":
 	main()
